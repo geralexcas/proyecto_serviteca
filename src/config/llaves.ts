4871912868c4833c7080af37d0d4cbba1f4699d0
geralexcas <geralexcas@gmail.com>
2022-11-15 18:17:54 -0500

    correo heroku Llaves.ts

--- src/config/llaves.ts
+++ src/config/llaves.ts
@@ -1,5 +1,6 @@
 export namespace Llaves {
   export const llaveJWT = 'JWT@2022*';
-  export const urlServicioCorreo = 'http://127.0.0.1:5000';
+  /*export const urlServicioCorreo = 'http://127.0.0.1:5000'; */
+  export const urlServicioCorreo = 'https://sendgridvirtual.herokuapp.com/';
   export const cadenaConexion = 'mongodb+srv://geralexcasmagno:kg9OVGMV37BleWNz@clusterger.n8jw38h.mongodb.net/proyecto?retryWrites=true&w=majority'
 }
